9022e48773d7dfc41c1780911a612a46aba7ef0b
Gopal Kaul <gopalkaul5@gmail.com>
2020-10-24 14:56:40 +0530

    Completed the app, added remove and add functions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import Todos from './Todos';
+import AddTodo from './AddForm';
+
 class App extends Component {
   state = {
     todos: [
-      { id: 1, content: 'Learn DRF' },
-      { id: 2, content: 'Learn React' },
     ]
   }
   deleteTodo = (id) => {
     const todos = this.state.todos.filter(todo => {
-      return todo.id!==id
+      return todo.id !== id
     });
     this.setState({
-      todos:todos,
+      todos: todos,
+    })
+  }
+  addTodo = (todo) => {
+    todo.id = this.state.todos.length + 1;
+    let todos = [...this.state.todos , todo];
+    this.setState({
+      todos
     })
   }
   render() {
@@ -20,6 +27,7 @@ class App extends Component {
       <div className='App container'>
         <h1 className='center blue-text'>To Dos</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     )
   }
